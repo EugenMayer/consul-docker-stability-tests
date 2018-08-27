a9e79a10f0f64bcc7cd5842249ea6fc622a1e896
Eugen Mayer <eugen.mayer@kontextwork.de>
2018-08-27 10:42:43 +0200

    Fix certificate age when auto generating

--- acls/bin/server_tls.sh
+++ acls/bin/server_tls.sh
@@ -38,9 +38,9 @@ mkdir -p "$SSL_DIR"
 # Generate our Private Key, CSR and Certificate
 # consul NEEDS a CA signed certificate, since we can only trust CAs but not certificates, running into
 # consul: error getting server health from "consulserver": rpc error getting client: failed to get conn: x509: certificate signed by unknown authority (possibly because of "crypto/rsa: verification error" while trying to verify candidate authority certificate "127.0.0.1")
-openssl req -nodes -x509 -newkey rsa:2048 -keyout ${SSL_DIR}/ca.key -out ${SSL_DIR}/ca.crt -subj "$(echo -n "$SUBJ" | tr "\n" "/")"
+openssl req -nodes -days 1825 -x509 -newkey rsa:2048 -keyout ${SSL_DIR}/ca.key -out ${SSL_DIR}/ca.crt -subj "$(echo -n "$SUBJ" | tr "\n" "/")"
 openssl req -nodes -newkey rsa:2048 -keyout ${SSL_DIR}/tls.key -out ${SSL_DIR}/cert.csr -subj "$(echo -n "$SUBJ" | tr "\n" "/")"
-openssl x509 -req -in ${SSL_DIR}/cert.csr -CA ${SSL_DIR}/ca.crt -CAkey ${SSL_DIR}/ca.key -CAcreateserial -out ${SSL_DIR}/cert.crt
+openssl x509 -days 1825 -req -in ${SSL_DIR}/cert.csr -CA ${SSL_DIR}/ca.crt -CAkey ${SSL_DIR}/ca.key -CAcreateserial -out ${SSL_DIR}/cert.crt
 
 # deploy our CA cert into the system ca root list
 cp ${SSL_DIR}/ca.crt /usr/local/share/ca-certificates/consul-ca.crt
